6455d9d1a62a77de53e8df1b65e5fae17bc7f7ba
ramatov-zulfikor <ramatovv@gmail.com>
2021-01-30 20:56:24 +0500

    tabs and search pane;

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
    $('.hero__slider').slick({
       dots: true,
       // autoplay: true,
@@ -6,4 +7,15 @@ $(document).ready(function () {
       nextArrow: '<button type="button" class="slider__btn slider__next btn-reset"><img src="images/icons/arrow-next.svg" alt=""></button>',
       arrows: true
    });
+
+   $('.tabs__item').click(function(e) {
+      e.preventDefault();
+
+      $('.tabs__item').removeClass('tabs__item--active')
+      $('.tabs-content__item').removeClass('tabs-content__item--active')
+
+      $(this).addClass('tabs__item--active')
+      $($(this).attr('href')).addClass('tabs-content__item--active')
+   })
+
 });
