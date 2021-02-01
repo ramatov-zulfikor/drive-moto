ebe462beb1e1f579e3ad5e9b1dd4c51c7b0fd10a
ramatov-zulfikor <ramatovv@gmail.com>
2021-02-01 14:26:56 +0500

    slider and tabs js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,19 @@ $(document).ready(function () {
       prevArrow: '<button type="button" class="slider__btn slider__prev btn-reset"><img src="images/icons/arrow-prev.svg" alt=""></button>',
       nextArrow: '<button type="button" class="slider__btn slider__next btn-reset"><img src="images/icons/arrow-next.svg" alt=""></button>',
       arrows: true
-   });
+   })
+
+   // ===
+
+   $('.products__slider').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      prevArrow: '<button type="button" class="slider__btn products__slider__prev btn-reset"><img src="images/icons/arrow-prev--black.svg" alt=""></button>',
+      nextArrow: '<button type="button" class="slider__btn products__slider__next btn-reset"><img src="images/icons/arrow-next--black.svg" alt=""></button>',
+      arrows: true
+   })
+
+   // ===
 
    $('.tabs__item').click(function(e) {
       e.preventDefault();
@@ -16,6 +28,26 @@ $(document).ready(function () {
 
       $(this).addClass('tabs__item--active')
       $($(this).attr('href')).addClass('tabs-content__item--active')
+   })
+
+   // ===
+
+   $('.product__wishlist-btn').click(function(e) {
+      e.preventDefault()
+
+      $(this).toggleClass('wishlist-btn--active')
+   })
+
+   // ===
+
+   $('.tabs-triggers__item').click(function(e) {
+      e.preventDefault();
+
+      $($(this).siblings()).removeClass('tabs-triggers__item--active')
+      $($(this).parent().siblings().find('.products-content__item')).removeClass('products-content__item--active')
+
+      $(this).addClass('tabs-triggers__item--active')
+      $($(this).attr('href')).addClass('products-content__item--active')
    })
 
 });
